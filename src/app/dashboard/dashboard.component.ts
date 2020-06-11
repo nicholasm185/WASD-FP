908fed1998b09b3255597017061b924b603345e4
amakumi <melvern3@gmail.com>
2020-06-11 16:37:50 +0700

    attempt on downloading CSV and proofs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,9 +5,10 @@ import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as AOS from 'aos';
+import * as fileSaver from 'file-saver';
 import { GetEventService } from '../services/get-event.service';
 import { Event } from '../interfaces/event';
-
+import { Http, ResponseContentType } from '@angular/http';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -18,6 +19,7 @@ export class DashboardComponent implements OnInit {
   user$: Observable<User>;
 
   events;
+  event_id;
   picturesrc: string;
 
   public User = [];
@@ -79,5 +81,73 @@ indexEvents() {
     alert('Please check your Email!');
   }
 
+
+  downloadCSV3(event_id): Observable<any> {
+    console.log('hello?');
+    event_id = this.setEventId();
+    return this.http.post('https://backend.ticketmaya.me/api/attendee/downloadCSV',
+    {
+      event_id: event_id,
+      responseType: ResponseContentType.Blob
+    });
+
+  }
+
+  downloadCSV2(event_id: string) {
+    this.get.downloadFile(event_id).subscribe(response => {
+      window.location.href = response.url;
+    }),
+      error => console.log('Error downloading the file'),
+        () => console.info('File downloaded successfully');
+  }
+
+  downloadCSV(event_id) {
+    this.get.downloadFile(event_id).subscribe(response => {
+      // let blob: any = new Blob([response.blob()], { type: 'text/json; charset=utf-8' });
+      // const url = window.URL.createObjectURL(blob);
+      // window.open(url);
+      // window.location.href = response.url;
+      this.downloadFile(response);
+      // fileSaver.saveAs(blob, 'employees.json');
+    }),
+    error => console.log('Error downloading the file'),
+    () => console.info('File downloaded successfully');
+  }
+
+  downloadFile(data: Response) {
+    const blob = new Blob([data], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(blob);
+    window.open(url);
+  }
+
+  /*downloadProof() {
+    this.get.downloadPaymentProof(this.event_id).subscribe((data: any) => {
+      this.event_id = data['event_id'];
+    },
+    error => {
+      console.log(error);
+      alert('Failed to download');
+    });
+  }*/
+
+  setEventId() {
+    this.event_id = this.events['event_id'];
+  }
+
+  getEventId() {
+    return this.events['event_id'];
+  }
+
+  dwZip(event_id) {
+    this.get.downloadPaymentProof(event_id).subscribe(data => {
+      //this.event_id = this.getEventId();
+      console.log('hi', data);
+    });
+  }
+
+  downloadZip(event_id: string) {
+    return this.http.get('https://backend.ticketmaya.me/api/attendee/dproof/' + {event_id, responseType: ResponseContentType.ArrayBuffer})
+      .subscribe(res => res);
+  }
 
 }
--- src/app/services/get-event.service.ts
+++ src/app/services/get-event.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpEventType, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-
+import { Http, ResponseContentType } from '@angular/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +10,8 @@ export class GetEventService {
   private url = 'https://backend.ticketmaya.me/api/events/show/';
   private showEventsUrl = 'https://backend.ticketmaya.me/api/events/showAll';
   private cancelUrl = 'https://backend.ticketmaya.me/api/events/delete/';
+  private downloadCSV = 'https://backend.ticketmaya.me/api/attendee/downloadCSV';
+  private downloadProof = 'https://backend.ticketmaya.me/api/attendee/dproof/';
 
   constructor(public http: HttpClient) { }
 
@@ -22,6 +25,14 @@ export class GetEventService {
 
   cancelEvent(event_id: string) {
     return this.http.get(this.cancelUrl + event_id);
+  }
+
+  downloadPaymentProof(event_id: string): Observable<any> {
+    return this.http.get(this.downloadProof + {event_id, responseType: ResponseContentType.ArrayBuffer});
+  }
+
+  downloadFile(event_id: string): Observable<any> {
+    return this.http.post(this.downloadCSV, {event_id, responseType: ResponseContentType.Blob});
   }
 
 }
